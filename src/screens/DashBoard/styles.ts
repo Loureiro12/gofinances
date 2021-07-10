1e5c5b6e09785986548794347ec68de5eeadb06a
André Loureiro <contato.loureiro1@gmail.com>
2021-07-09 22:49:06 -0300

    RectButton e BorderlessButton

--- src/screens/DashBoard/styles.ts
+++ src/screens/DashBoard/styles.ts
@@ -6,6 +6,7 @@ import {
   getStatusBarHeight,
   getBottomSpace,
 } from "react-native-iphone-x-helper";
+import { BorderlessButton } from "react-native-gesture-handler";
 
 import { DataListProps } from ".";
 
@@ -63,6 +64,8 @@ export const UserName = styled.Text`
   font-size: ${RFValue(16)}px;
   font-family: ${({ theme }) => theme.fonts.bold};
 `;
+
+export const LogoutButton = styled(BorderlessButton)``;
 
 export const Icon = styled(Feather)`
   color: ${({ theme }) => theme.colors.secondary};

738d47554ff8b4886ac6efd2e48813a40f4c6dd0
André Loureiro <contato.loureiro1@gmail.com>
2021-07-29 16:37:56 -0300

    Finalizando a lista

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -54,7 +54,7 @@ export function Register() {
     formState: { errors },
   } = useForm({ resolver: yupResolver(schema) });
 
-  function handleTransactionTypesSelect(type: "up" | "down") {
+  function handleTransactionTypesSelect(type: "positive" | "negative") {
     setTransactionType(type);
   }
 
@@ -76,7 +76,7 @@ export function Register() {
       id: String(uuid.v4()),
       name: form.name,
       amount: form.amount,
-      transactionType,
+      type: transactionType,
       category: category.key,
       date: new Date(),
     };
@@ -151,14 +151,14 @@ export function Register() {
               <TransactionTypeButton
                 type="up"
                 title="Income"
-                onPress={() => handleTransactionTypesSelect("up")}
-                isActive={transactionType === "up"}
+                onPress={() => handleTransactionTypesSelect("positive")}
+                isActive={transactionType === "positive"}
               />
               <TransactionTypeButton
                 type="down"
                 title="Outcome"
-                onPress={() => handleTransactionTypesSelect("down")}
-                isActive={transactionType === "down"}
+                onPress={() => handleTransactionTypesSelect("negative")}
+                isActive={transactionType === "negative"}
               />
             </TransactionTypes>
             <CategorySelectButton

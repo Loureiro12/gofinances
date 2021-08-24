66d469c1f493193d3ea473cc491ba45c16b84f0c
André Loureiro <contato.loureiro1@gmail.com>
2021-08-24 11:35:04 -0300

    Criando nosso próprio Hook

---
+++ src/hooks/auth.tsx
@@ -0,0 +1,38 @@
+import React, { useContext, createContext, ReactNode } from "react";
+
+interface AuthProviderProps {
+  children: ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface AuthContextData {
+  user: User;
+}
+
+const AuthContext = createContext({} as AuthContextData);
+
+function AuthProvider({ children }: AuthProviderProps) {
+  const user = {
+    id: "5243523",
+    name: "André Loureiro",
+    email: "contato",
+  };
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  );
+}
+
+function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
+
+export { AuthProvider, useAuth };

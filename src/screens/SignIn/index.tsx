3550d3ebfd8e69ab55349248250dcf31376ac22f
André Loureiro <65689062+Loureiro12@users.noreply.github.com>
2021-09-19 11:58:48 -0300

    Conectando com uma Conta Apple

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Alert } from "react-native";
-import { RFValue } from "react-native-responsive-fontsize";
+import React from 'react';
+import { Alert } from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
 
-import AppleSvg from "../../assets/apple.svg";
-import GoogleSvg from "../../assets/google.svg";
-import LogoSvg from "../../assets/logo.svg";
+import AppleSvg from '../../assets/apple.svg';
+import GoogleSvg from '../../assets/google.svg';
+import LogoSvg from '../../assets/logo.svg';
 
-import { useAuth } from "../../hooks/auth";
+import { useAuth } from '../../hooks/auth';
 
-import { SignInSocialButton } from "../../components/SignInSocialButton";
+import { SignInSocialButton } from '../../components/SignInSocialButton'
 
 import {
   Container,
@@ -17,18 +17,27 @@ import {
   Title,
   SignInTitle,
   Footer,
-  FooterWrapper,
-} from "./styles";
+  FooterWrapper
+ } from './styles';
 
 export function SignIn() {
-  const { signInWithGoogle } = useAuth();
+  const { signInWithGoogle, signInWithApple } = useAuth();
 
   async function handleSignInWithGoogle() {
     try {
       await signInWithGoogle();
     } catch (error) {
       console.log(error);
-      Alert.alert("Não foi possível conectar a conta google");
+      Alert.alert('Não foi possível conectar a conta Google');
+    }
+  }
+
+  async function handleSignInWithApple() {
+    try {
+      await signInWithApple();
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Não foi possível conectar a conta Apple');
     }
   }
 
@@ -36,17 +45,20 @@ export function SignIn() {
     <Container>
       <Header>
         <TitleWrapper>
-          <LogoSvg width={RFValue(120)} height={RFValue(68)} />
-
+          <LogoSvg
+            width={RFValue(120)}
+            height={RFValue(68)}
+          />
+          
           <Title>
-            Controle suas {`\n`}
-            finanças de forma {"\n"}
+            Controle suas {'\n'}
+            finanças de forma {'\n'}
             muito simples
           </Title>
         </TitleWrapper>
 
         <SignInTitle>
-          Faça seu login com {"\n"}
+          Faça seu login com {'\n'}
           uma das contas abaixo
         </SignInTitle>
       </Header>
@@ -54,11 +66,16 @@ export function SignIn() {
       <Footer>
         <FooterWrapper>
           <SignInSocialButton
-            title="Entrar com google"
+            title="Entrar com Google"
             svg={GoogleSvg}
             onPress={handleSignInWithGoogle}
           />
-          <SignInSocialButton title="Entrar com apple" svg={AppleSvg} />
+
+          <SignInSocialButton
+            title="Entrar com Apple"
+            svg={AppleSvg}
+            onPress={handleSignInWithApple}
+          />
         </FooterWrapper>
       </Footer>
     </Container>
